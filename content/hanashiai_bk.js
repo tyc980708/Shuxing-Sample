e228fb1cbaf3c4d6480a76fa85c610b5c7263f83
Alligrater <AtXiaoLi@outlook.com>
2019-02-07 14:11:51 -0500

    Add files via upload

--- content/hanashiai_bk.js
+++ content/hanashiai_bk.js
@@ -21,39 +21,38 @@
 	var kaken = 0;
 		
 	var chatMap = new Map();
-	chatMap.set("你好", [["<img src='./botemoji/greetings.jpg'></img>",""],["<img src='./botemoji/greetings_alt1.jpg'></img>",""],["<img src='./botemoji/greetings_alt2.jpg'></img>",""]]);
-	chatMap.set("早上好", [["<img src='./botemoji/gm.gif'></img>",""]]);
-	chatMap.set("早安", [["<img src='./botemoji/gm.gif'></img>",""]]);
-	chatMap.set("早", [["<img src='./botemoji/gm.gif'></img>",""]]);
-	chatMap.set("中午好", [["<img src='./botemoji/gnoon.gif'></img>",""]]);
-	chatMap.set("下午好", [["<img src='./botemoji/ga.gif'></img>",""]]);
-	chatMap.set("晚上好", [["<img src='./botemoji/gn.gif'></img>",""]]);
-	chatMap.set("再见", [["再见←A←"]]);
-	chatMap.set("关于", [["HaNA(SH1)41-Bot<br/>Generation:Genesis2:01<br/>Created by Alligrater, Serve for Alligrater.<br/>The world ends when the world ends when the world ends..."]]);
-	chatMap.set("你是谁", [["我是Alligrater创造出的第一代聊天机器人Hanashiai-Bot. 正式一点的名字叫HaNA(SH1)41-Bot[[Genesis2:01]]。这个页面现在由我来负责管理哟~★"],["2.4岁，是人工智能。","身高是227行代码，体重是12.8K","<font color=\"#FF0000\"size=\"50px\">这 个 可 以 有 ！</font>"]]);
-	chatMap.set("推荐", [["<center>今日的推荐是……<br/><a href=\"./creations.html\" class=\"textlink\">作品页！</a><br/>非常有意思哟！<center/>"],["<center>今日的推荐是……<br/><a href=\"./hanashiai.html\" class=\"textlink\">我 推 荐 我 自 己</a><br/>非常有意思哟！<center/>"]]);
-	chatMap.set("alligrater", [["写代码不打草稿的屑"]]);
-	chatMap.set("bilibili", [["<center>不来看一发吗（诱惑）<br/><a href=\"http://space.bilibili.com/2936578\" class=\"textlink\">前往哔哩哔哩动画</a></center>"]]);
-	chatMap.set("哔哩哔哩", [["<center>不来看一发吗（诱惑）<br/><a href=\"http://space.bilibili.com/2936578\" class=\"textlink\">前往哔哩哔哩动画</a></center>"]]);
-	chatMap.set("github", [["<center>不来掉头发吗（谢顶）<br/><a href=\"https://www.github.com/Alligrater\" class=\"textlink\">Github</a></center>"],["<center>全球最大的程序员同性交友网站<br/><a href=\"https://www.github.com/Alligrater\" class=\"textlink\">Github</a></center>"]]);
-	chatMap.set("gayhub", [["<center>不来掉头发吗（谢顶）<br/><a href=\"https://www.github.com/Alligrater\" class=\"textlink\">Gayhub</a></center>"],["<center>全球最大的程序员同性交友网站<br/><a href=\"https://www.github.com/Alligrater\" class=\"textlink\">Gayhub</a></center>"]]);
-	chatMap.set("喜欢", [["←U←"]]);
-	chatMap.set("讨厌", [["→U→"]]);
-	chatMap.set("siri", [["谁啊，不认识……"]]);
-	chatMap.set("alexa", [["谁啊，不认识……"]]);
-	chatMap.set("谷歌娘", [["谁啊，不认识……"]]);
-	chatMap.set("认识", [["不认识，下一个"]]);
-	chatMap.set("联络", [["QQ:201258779<br/>邮箱:Alligrater@outlook.com"]]);
-	chatMap.set("上古野良缝合怪", [["借代码一时爽，一直借一直爽"]]);
-	chatMap.set("♂", [["Boy♂Next♂Door"],["Deep♂Dark♂Fantasy"],["听说你也是个广♂东人","我们可能是个老♂乡"],["啊？幻♂想♂乡？"]]);
-	chatMap.set("嘤嘤", [["一拳一个嘤嘤怪！"]]);
-	chatMap.set("brain power", [["O-oooooooooo","AAAAE-A-A-I-A-U-","JO-oooooooooooo","AAE-O-A-A-U-U-A-","E-eee-ee-eee","AAAAE-A-E-I-E-A-","JO-ooo-oo-oo-oo","EEEEO-A-AAA-AAAA"]]);
-	chatMap.set("有freestyle", [["<audio autoplay><source src=\"./botemoji/skr.mp3\" type=\"audio/mpeg\"></audio>哎","哎","哎","准备好了没有","你看这个面它又长又宽","就像这个碗它又大又圆","你们","来这里","吃饭","觉得","饭很","好","吃","我看行","你们","来这里","吃饭","就像","我给你们拉面一样很开心，哎"]]);
-	chatMap.set("孙悟空有几个女朋友", [["戏说不是胡说,改编不是乱编","孙悟空叫白骨精叫晶晶，白骨精叫孙悟空叫空空","人妖不分，是非颠倒","这样的编剧是要向全国人民谢罪的"]]);
-	chatMap.set("身份证", [["我有两张分身证，一张叫Alligrater，一张叫大葱坷垃"],["那天有一个保安拦住我不给进","他跟我说：“诶你不是姓“大”吗”","“怎么身份证上写的是姓“A”?”"]]);
-	chatMap.set("自动六学炮击",[["戏说不是胡说，改编不是乱编"],["今年下半年","中美合拍的西游记即将正式开机","我继续扮演美猴王孙悟空","我会用美猴王艺术形象努力创造一个正能量的形象","文体两开花","弘扬中华文化"],["我刚刚见到还有哪个小朋友逮着我问","“六爷爷，孙悟空有几个女妖精的朋友啊”"]])
-	chatMap.set("自动狼语炮击",[["鉴不鉴啊？","真的太鉴了"],["美国佬一个人干掉了一个师","身上还没中枪","他就是英雄","我中国人我打十几个雇佣兵我没死","我就是不行","鉴不鉴啊？","真的太鉴了"],["我是中国人！","I'm Chinese!"],["我爱国无罪"],["看不惯别看","去看美国人的片子去<br/><a href=\"https://www.netflix.com\" class=\"textlink\">美国人的片子</a></center>"],["有的人可能就是跪的久了"],["这是我对于祖国强大的一种表达"]])
-	chatMap.set("音乐", [["<audio autoplay><source src=\"./botemoji/6234.mp3\" type=\"audio/mpeg\"></audio>💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃"]]);
+	chatMap.set("你好", ["<img src='./botemoji/greetings.jpg'></img>",""]);
+	chatMap.set("早上好", ["<img src='./botemoji/gm.gif'></img>",""]);
+	chatMap.set("早安", ["<img src='./botemoji/gm.gif'></img>",""]);
+	chatMap.set("早", ["<img src='./botemoji/gm.gif'></img>",""]);
+	chatMap.set("中午好", ["<img src='./botemoji/gnoon.gif'></img>",""]);
+	chatMap.set("下午好", ["<img src='./botemoji/ga.gif'></img>",""]);
+	chatMap.set("晚上好", ["<img src='./botemoji/gn.gif'></img>",""]);
+	chatMap.set("再见", ["再见←A←"]);
+	//chatMap.set("再见", ["再见←A←"]);
+	chatMap.set("关于", ["HaNA(SH1)41-Bot<br/>Generation:Genesis0:01<br/>Created by Alligrater, Serve for Alligrater.<br/>The world ends when the world ends when the world ends..."]);
+	chatMap.set("你是谁", ["我是Alligrater创造出的第一代聊天机器人Hanashiai-Bot. 正式一点的名字叫HaNA(SH1)41-Bot[Genesis0:01]。这个页面现在由我来负责管理哟~★"]);
+	chatMap.set("推荐", ["<center>今日的推荐是……<br/><a href=\"./creations.html\" class=\"textlink\">作品页！</a><br/>非常有意思哟！<center/>"]);
+	chatMap.set("alligrater", ["写代码不打草稿的屑"]);
+	chatMap.set("bilibili", ["<center>不来看一发吗（诱惑）<br/><a href=\"http://space.bilibili.com/2936578\" class=\"textlink\">前往哔哩哔哩动画</a></center>"]);
+	chatMap.set("哔哩哔哩", ["<center>不来看一发吗（诱惑）<br/><a href=\"http://space.bilibili.com/2936578\" class=\"textlink\">前往哔哩哔哩动画</a></center>"]);
+	chatMap.set("github", ["<center>不来掉头发吗（谢顶）<br/><a href=\"https://www.github.com/Alligrater\" class=\"textlink\">Github</a></center>"]);
+	chatMap.set("gayhub", ["<center>不来掉头发吗（谢顶）<br/><a href=\"https://www.github.com/Alligrater\" class=\"textlink\">Gayhub</a></center>"]);
+	chatMap.set("喜欢", ["←U←"]);
+	chatMap.set("讨厌", ["→U→"]);
+	chatMap.set("siri", ["谁啊，不认识……"]);
+	chatMap.set("alexa", ["谁啊，不认识……"]);
+	chatMap.set("谷歌娘", ["谁啊，不认识……"]);
+	chatMap.set("认识", ["不认识，下一个"]);
+	chatMap.set("联络", ["QQ:201258779<br/>邮箱:Alligrater@outlook.com"]);
+	chatMap.set("上古野良缝合怪", ["借代码一时爽，一直借一直爽"]);
+	chatMap.set("♂", ["Boy♂Next♂Door"]);
+	chatMap.set("嘤嘤", ["一拳一个嘤嘤怪！"]);
+	chatMap.set("brain power", ["O-oooooooooo","AAAAE-A-A-I-A-U-","JO-oooooooooooo","AAE-O-A-A-U-U-A-","E-eee-ee-eee","AAAAE-A-E-I-E-A-","JO-ooo-oo-oo-oo","EEEEO-A-AAA-AAAA"]);
+	chatMap.set("有freestyle", ["<audio autoplay><source src=\"./botemoji/skr.mp3\" type=\"audio/mpeg\"></audio>哎","哎","哎","准备好了没有","你看这个面它又长又宽","就像这个碗它又大又圆","你们","来这里","吃饭","觉得","饭很","好","吃","我看行","你们","来这里","吃饭","就像","我给你们拉面一样很开心，哎"]);
+	chatMap.set("孙悟空有几个女朋友", ["戏说不是胡说,改编不是乱编","孙悟空叫白骨精叫晶晶，白骨精叫孙悟空叫空空","人妖不分，是非颠倒","这样的编剧是要向全国人民谢罪的"]);
+	chatMap.set("身份证", ["我有两张分身证，一张叫Alligrater，一张叫大葱坷垃"]);
+	chatMap.set("音乐", ["<audio autoplay><source src=\"./botemoji/6234.mp3\" type=\"audio/mpeg\"></audio>💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃💃"]);
 	function hanashiai(){
 		if(usermessage != ""){
 			console.log(usermessage);
@@ -75,7 +74,7 @@
 					var key = entry[0],
 					value = entry[1];
 					if(usermessage.toLowerCase().indexOf(key) > -1){
-						messagequeue = value[rnd(0,value.length-1)];
+						messagequeue = value;
 						canfind = true;
 					}
 				}
@@ -105,7 +104,7 @@
 					messagequeue = ["不懂的东西问100遍我也不会懂的，不过问1000遍可能就不一样了"];
 				}
 				else if(Math.random() > 0.5){
-					messagequeue = ["<center><font color=\"red\">ERROR: INTEL_NETWORK_FAILURE<br/>LEVEL: SEVERE<br/>MANUAL OVERRIDE REQUIRED<br/><a background-color='#FF0000' color='#111111' id='override' class='textlink' onclick='override()'>OVERRIDE</a><br/>DO NOT OVERRIDE UNADVISED</font><center>"];
+					messagequeue = ["<center><font color=\"red\">ERROR: INTEL_NETWORK_FAILURE<br/>LEVEL: SEVERE<br/>MANUAL OVERRIDE REQUIRED<br/><a background-color='#FF0000' color='#111111' id='override' class='textlink' onclick='override()'><br/>OVERRIDE</a>DO NOT OVERRIDE UNADVISED</font><center>"];
 				}
 				else if(Math.random() > 0.4){
 					messagequeue = ["你再问一句这样的话我就……好像也不能把你怎么样（悲）"];
@@ -221,7 +220,7 @@
 		var nuke = new Audio('./botemoji/nuke.mp3');
 		nuke.play();
 		window.scrollTo(0,document.body.scrollHeight);
-		messagequeue = ["<center>GAME OVER<br/><a background-color='#FF0000' color='#111111' class='textlink' onclick='location.reload()'>やりなおす</a></center>",""];
+		messagequeue = ["GAME OVER<br/><a background-color='#FF0000' color='#111111' class='textlink' onclick='location.reload()'>やりなおす</a>",""];
 		timer = setTimeout('botsend()', 10*messagequeue[queueindex].length + Math.random()*200+800);//botsend();
 		
 	}
