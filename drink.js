4a2ddb6e55da9f084d51b77195817f0be86995d4
Alligrater <AtXiaoLi@outlook.com>
2020-10-18 21:47:37 -0400

    added drink

---
+++ drink.js
@@ -0,0 +1,83 @@
+
+import {GLTFLoader} from "./modules/GLTFLoader.js";
+import {OrbitControls} from "./modules/OrbitControls.js";
+import * as THREE from "./modules/three.module.js"
+
+
+let scene;
+let camera;
+let renderer;
+let gltfLoader, textureLoader;
+let drinkModel;
+let drinkMaterial;
+
+function init(){
+    //init the scene... basically the manual from threejs.org
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(
+        60, window.innerWidth / window.innerHeight,
+        0.1, 5000
+    );
+    //So the background is clear
+    renderer = new THREE.WebGLRenderer({ alpha:true });
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement);
+    let controls = new OrbitControls( camera, renderer.domElement );
+
+    //Register for window resize events:
+    window.addEventListener('resize', onWindowResize, false);
+    window.addEventListener('mousemove', onMouseMove, false);
+
+
+    //Create loaders
+    gltfLoader = new GLTFLoader();
+    textureLoader = new THREE.TextureLoader();
+
+    //Make unlit texture:
+    textureLoader.load('models/Nile.png', function(texture) {
+        texture.magFilter = THREE.NearestFilter
+        drinkMaterial = new THREE.MeshBasicMaterial( {
+            map: texture
+        });
+    }, undefined, function(error){
+        console.error(error);
+    });
+
+    //Load, then call back maybe
+    gltfLoader.load('models/Nile.glb', function(gltf){
+        gltf.scene.traverse( function ( child ) {
+            if ( child.isMesh ) {
+                child.geometry.center(); // center here
+                child.material = drinkMaterial;
+            }
+        });
+        scene.add(gltf.scene);
+        camera.position.z = 0.5;
+
+    }, undefined, function (error) {
+        console.error(error);
+    });
+    update();
+}
+
+
+
+
+function update(){
+    renderer.render(scene, camera);
+    //cube.rotation.y += 0.1;
+    requestAnimationFrame(update);
+}
+
+function onWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function onMouseMove(event){
+    //Calculate out the normalized position of the mouse:
+    console.log(event.movementX);
+}
+
+init();
