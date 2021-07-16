beef88a4da011447e8ed5c02920c8a5ae3e7b030
Alligrater <Alligrater@outlook.com>
2021-07-15 22:29:41 -0400

    Add files via upload

---
+++ Ball/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1626402517,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/panel1-sheet0.png",
+		"images/score1-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/panel3-sheet0.png",
+		"images/score3-sheet0.png",
+		"images/9patch.png",
+		"images/particles.png",
+		"images/spritefont.png",
+		"images/fnt_black-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"penumbra.png"
+	]
+}
