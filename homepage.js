81ba9eeccde2247ef55684dde0d9d54a4e451db8
DeveloGrater <Alligrater@outlook.com>
2022-04-05 22:25:27 -0400

    sole

---
+++ homepage.js
@@ -0,0 +1,30 @@
+
+
+const randomLoadText = [
+    "Loading...",
+    "Trapping Souls...",
+    "Reticulating Spines...",
+    "Consuming Humans...",
+    "Grating Flesh...",
+    "Dialing Up...",
+    "Commencing Ritual...",
+]
+
+const loadedText = "SHUXING LI"
+
+let timeElapsed = 0;
+
+function init() {
+  console.log('homepage.js loaded');
+  //set the text randomly
+  //every 0.5 seconds
+  //for a total of 2 seconds.
+  setInterval(function() {
+    let randomIndex = Math.floor(Math.random() * randomLoadText.length);
+    timeElapsed += 0.15;
+    document.getElementById("loadtext").innerHTML = randomLoadText[randomIndex];
+    if(timeElapsed >= 1.5) {
+      document.getElementById("loadtext").innerHTML = loadedText;
+    }
+  }, 100);
+}
