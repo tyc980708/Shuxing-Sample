ab7d22277dc9787d2b3f11c9cd9c1fd165b4f932
DeveloGrater <Alligrater@outlook.com>
2022-04-08 14:43:08 -0400

    dgtr

---
+++ dynatext.js
@@ -0,0 +1,45 @@
+let loadedJSON;
+let activeLanguage = "none";
+
+function fetchDynatext(language){
+    activeLanguage = language;
+    let dynatextDescFile = "lang-" + language + ".json"
+    //fetch file...
+    fetch(dynatextDescFile).then(function(response){
+        updateDynatext(response.json().then(
+            data => updateDynatext(data)))
+    }).catch(error => console.log("failed to fetch language json."))
+}
+
+function fetchDynatextString(language, key){
+    if(activeLanguage !== language){
+        fetchDynatext(language);
+    }
+    else{
+        //lookup from the json:
+        return loadedJSON[key];
+    }
+}
+
+function fetchString(key){
+    return fetchDynatextString(activeLanguage, key);
+}
+
+function updateDynatext(dynatextDescriptor){
+    console.log(dynatextDescriptor)
+    loadedJSON = dynatextDescriptor;
+    let dynatextContainers = document.getElementsByClassName("dynatext")
+    for(let i = 0; i < dynatextContainers.length; i++){
+        let item = dynatextContainers.item(i)
+        let attr = item.getAttribute("langdesc")
+        if(attr){
+            let dynatextContent = dynatextDescriptor[attr]
+            if(dynatextContent){
+                item.innerHTML = dynatextContent
+                //console.log("updated text to" + dynatextContent)
+            }
+        }
+    }
+}
+
+
--- homepage.js
+++ homepage.js
@@ -15,19 +15,46 @@ const loadedText = "SHUXING LI"
 let timeElapsed = 0;
 
 function init() {
-  console.log('homepage.js loaded');
-  //set the text randomly
-  //every 0.5 seconds
-  //for a total of 2 seconds.
-  setInterval(function() {
-    let randomIndex = Math.floor(Math.random() * randomLoadText.length);
-    timeElapsed += 0.15;
-    document.getElementById("loadtext").innerHTML = randomLoadText[randomIndex].toUpperCase();
-    if(timeElapsed >= 1.0) {
-      document.getElementById("loadtext").innerHTML = loadedText;
-      clearInterval(this);
-    }
-  }, 100);
+    //set the text randomly
+    //every 0.5 seconds
+    //for a total of 2 seconds.
+    fetchDynatext("en")
+    buttonListenMouseHover();
+    updateLoadingText();
+}
+
+function buttonListenMouseHover(){
+    //for each of the dom element with class "button"
+    //add a mouseover event listener
+    //that appends a ">" at the start
+    document.querySelectorAll(".future-button").forEach(button => {
+        button.addEventListener("mouseover", onMouseHoverButton);
+        button.addEventListener("mouseout", onMouseUnhoverButton);
+    });
+}
+
+function onMouseHoverButton(){
+    let lang = this.getAttribute("langdesc");
+    let localeString = fetchString(lang);
+    this.innerHTML = "> " + localeString;
+}
+
+function onMouseUnhoverButton(){
+    let lang = this.getAttribute("langdesc");
+    let localeString = fetchString(lang);
+    this.innerHTML = localeString;
+}
+
+function updateLoadingText(){
+    setInterval(function() {
+        let randomIndex = Math.floor(Math.random() * randomLoadText.length);
+        timeElapsed += 0.15;
+        document.getElementById("loadtext").innerHTML = randomLoadText[randomIndex].toUpperCase();
+        if(timeElapsed >= 1.0) {
+            document.getElementById("loadtext").innerHTML = loadedText;
+            clearInterval(this);
+        }
+    }, 100);
 }
 
 function update() {
